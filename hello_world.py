32d201e91dc8bc02b18f2d7a799f92d6226b0d0e
Yasith-Mayura <mayurayasith@gmail.com>
2024-06-28 11:23:02 +0530

    cipher programme first commit

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+text = 'Hello World'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+encrypted_text = ''
+
+for char in text.lower():
+    print(char == ' ')
+    index = alphabet.find(char)
+    new_index = index + shift
+    encrypted_text += alphabet[new_index]
+    print('char:', char, 'encrypted text:', encrypted_text)
